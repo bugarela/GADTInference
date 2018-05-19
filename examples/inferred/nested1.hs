cb7854130530900c73902ddee2555bc4c1ab0759
Gabriela Moreira <gabrielamoreira05@gmail.com>
2018-05-19 17:27:02 -0300

    Something was missplaced

--- examples/inferred/nested1.hs
+++ examples/inferred/nested1.hs
@@ -1,3 +1,3 @@
-data T a where {TInt :: (a ~ Bool) => b -> T a; TAny :: T a}
-e = let f = (\x -> case x of {TInt n -> (case n of {TInt y -> y > 1});TAny -> True}) in f
+data P a where {P1 :: (a ~ Bool) => b -> P a; P2 :: P a}
+e = let f = (\x -> case x of {P1 n -> (case n of {P1 y -> y > 1});P2 -> True}) in f
 -- b is a rigid type variable. Maybe this is wrong.
